3a0822a7817c193b50598af8da011be415332d58
OYAMA YUKI <oja_yuki@mac.com>
2025-11-27 15:44:43 +0900

    controller new_html routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,4 @@
 Rails.application.routes.draw do
-  get 'texts/new'
-  get 'texts/create'
-  get 'texts/show'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
-
-  # Defines the root path route ("/")
-  # root "posts#index"
+  resources :texts, only: [:new, :create, :show]  
+  root "texts#new"
 end
